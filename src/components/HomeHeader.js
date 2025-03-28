8a51b715acf5ae47ed328189f2503c66b5b16933
Deep Sankar De <78144390+Deep-dsd@users.noreply.github.com>
2025-03-28 19:23:48 +0530

    card-component-commit

---
+++ src/components/HomeHeader.js
@@ -0,0 +1,10 @@
+const HomeHeader = ()=>{
+  return (
+    <section className="text-center">
+      <h1 className="text-6xl mb-2 text-white"><span className="font-bold">Hello</span>, Rating Master</h1>
+      <h4 className="text-3xl text-white/80 tracking-wider">Add your review </h4>
+    </section>
+  )
+}
+
+export default HomeHeader
