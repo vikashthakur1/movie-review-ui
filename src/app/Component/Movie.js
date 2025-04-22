d1aa8b8eb896946da67536612182d3f1fbaa1612
Vikash <vikastsr@gmail.com>
2025-04-22 10:50:55 +0530

    1st changes

---
+++ src/app/Component/Movie.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+const movies = [
+    {
+      id: 1,
+      name: "COCO",
+      price: "$12.99",
+      image: "/assets/images/coco.jpg",
+    },
+    
+    
+  ];
+  
+  export default function MovieCart() {
+    return (
+      
+       <h1></h1>
+    );
+  }
